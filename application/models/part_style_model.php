4ac74fd1bdb56edc2be5f05d2bdbd4d8351fbe22
khemmachart.r@apricotion.com <khemmachart.r@apricotion.com>
2014-03-17 02:47:24 +0700

    correct part_style pk

--- application/models/part_style_model.php
+++ application/models/part_style_model.php
@@ -13,9 +13,6 @@ Class Part_style_model extends Base_model
 
 		//array_push($this->after_get, 'after_get_mapping');
 
-		array_push($this->before_create, 'before_create_check_Key');
-
-		array_push($this->before_update, 'before_update_check_Key');
     }
 /*
 	public function after_get_mapping($o){
@@ -23,16 +20,6 @@ Class Part_style_model extends Base_model
 		return $o;
 	}
 */
-	public function before_create_check_Key($o){
-		$o['id'] = $o['code'];
-		unset($o['code']);
-		return $o;
-	}
-
-	public function before_update_check_Key($o){
-		unset($o['code']);
-		return $o;
-	}
 
 	protected $validate = array(
 		array(
